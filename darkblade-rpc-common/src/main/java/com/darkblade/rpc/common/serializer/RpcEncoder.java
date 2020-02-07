92ced70562ff04815545009a2683f70681811716
WIN-2EIHV9RNT97\Administrator <chenmutime@outlook.com>
2020-02-07 17:47:01 +0800

    添加注释

--- darkblade-rpc-common/src/main/java/com/darkblade/rpc/common/serializer/RpcEncoder.java
+++ darkblade-rpc-common/src/main/java/com/darkblade/rpc/common/serializer/RpcEncoder.java
@@ -12,6 +12,13 @@ public class RpcEncoder extends MessageToByteEncoder {
         this.genericClass = genericClass;
     }
 
+    /**
+     * 在执行编码之前，netty会先分配一块内存（直接内存或堆内存），即ByteBuf，用于填充编码后的数据。如果是堆内存，则会发生一次数据拷贝操作。
+     * @param channelHandlerContext
+     * @param obj
+     * @param byteBuf
+     * @throws Exception
+     */
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, Object obj, ByteBuf byteBuf) throws Exception {
         if(genericClass.isInstance(obj)){
